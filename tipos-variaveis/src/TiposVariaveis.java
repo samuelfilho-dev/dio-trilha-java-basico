213ab5738f4346ccee23370b01af3b2972a2ca6a
samuraiDosDados <snamuelfilho2011@gmail.com>
2022-10-29 15:21:09 -0300

    Aulas de tipos e Variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,31 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // "." é usado como separação decimal
+        double salarioMinimo = 2.500;
+
+        // Exemplo de Mundança de Variavel
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14; // Se torna uma Cosntante com o "Final"
+
+    }
+    public class TiposDados{
+        // Exemplos de Variaveis Numericas
+//        byte idade = 123;
+//        short ano = 2021;
+//        int cep = 21070333; --Não começar com Zero
+//        long cpf = 98765432109L; --Não começar com Zero -- Terninar com L
+//        float pi = 3.14F; --Precisa terminar com F
+//        double salario = 1275.33;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        // **ESTUDAR**
+        // Tipos Primitivos
+        // Classe Sting
     }
 }
+
