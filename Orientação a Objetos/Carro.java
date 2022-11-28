5fa5e94b25692ff54486c9af7ce37c18aabe5f83
Samuel Marques <snamuelfilho2011@gmail.com>
2022-11-28 20:33:44 -0300

    Aula 3 - Criação de Atributos

--- Orientação a Objetos/Carro.java
+++ Orientação a Objetos/Carro.java
@@ -1,4 +1,11 @@
 // Classe de exemplo do Exercicio 3
 public class Carro {
 
+    // Atributos Da Classe - Aula 3
+    String cor;
+    String modelo;
+    double capaciadadeDoTanque;
+
+    Carro(){}
+
 }
