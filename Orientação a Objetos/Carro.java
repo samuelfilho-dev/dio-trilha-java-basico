e67d543ef2f15ce22919ac3b73cf1d0cd8e7dc66
Samuel Marques <snamuelfilho2011@gmail.com>
2022-11-28 21:12:31 -0300

    Aula 3 - Criação de Métodos

--- Orientação a Objetos/Carro.java
+++ Orientação a Objetos/Carro.java
@@ -4,8 +4,45 @@ public class Carro {
     // Atributos Da Classe - Aula 3
     String cor;
     String modelo;
-    double capaciadadeDoTanque;
+    int capaciadadeDoTanque;
 
+
+    // Construtores
     Carro(){}
+    Carro(String cor, String modelo, int capaciadadeDoTanque){
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capaciadadeDoTanque = capaciadadeDoTanque;
+    }
+
+    // Métodos Getter e Setter
+    void setCor(String cor){
+        this.cor = cor;
+    }
+
+    String getCor(){
+        return cor;
+    }
+
+    void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    String getModelo(){
+        return modelo;
+    }
+
+    void setCapaciadadeDoTanque(int capaciadadeDoTanque){
+        this.capaciadadeDoTanque = capaciadadeDoTanque;
+    }
+
+    int getCapaciadadeDoTanque(){
+        return capaciadadeDoTanque;
+    }
+
+    // Método do Valor do tanque
+    double totalValorDoTanque(double valorDoCombustivel){
+        return capaciadadeDoTanque * valorDoCombustivel;
+    }
 
 }
