e08bf361cb2056b2006ae0cadf33c8f7af32df45
Samuel Marques <snamuelfilho2011@gmail.com>
2022-12-01 00:46:14 -0300

    Aula 4 - O.O - Interface

--- Orientação a Objetos/Heranca/Interface/Calculadora.java
+++ Orientação a Objetos/Heranca/Interface/Calculadora.java
@@ -1,2 +1,27 @@
-package Heranca.Interface;public class Calculadora {
+package Heranca.Interface;
+
+
+public class Calculadora implements OperacaoMatematica {
+
+    @Override
+    public void soma(double numero1, double numero2){
+        System.out.println("Soma: " + (numero1 + numero2));
+    }
+
+    @Override
+    public void subtracao(double numero1, double numero2){
+        System.out.println("Subtração: " + (numero1 - numero2));
+    }
+
+    @Override
+    public void multiplicacao(double numero1, double numero2){
+        System.out.println("Multiplicação: " + (numero1 * numero2));
+    }
+
+    @Override
+    public void divisao(double numero1, double numero2){
+        System.out.println("Divisão: " + (numero1 / numero2));
+    }
+
+
 }
--- Orientação a Objetos/Heranca/Interface/OperacaoMatematica.java
+++ Orientação a Objetos/Heranca/Interface/OperacaoMatematica.java
@@ -1,2 +1,10 @@
-package Heranca.Interface;public interface OperacaoMatematica {
+package Heranca.Interface;
+
+public interface OperacaoMatematica {
+
+    void soma(double numero1, double numero2);
+    void subtracao(double numero1, double numero2);
+    void multiplicacao(double numero1, double numero2);
+    void divisao(double numero1, double numero2);
+
 }
